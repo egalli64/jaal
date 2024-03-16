505ecb0a50f8f30b69396dadf00e22af69cad3bc
Emanuele <egalli64@gmail.com>
2024-03-16 19:21:16 +0100

    reverse in place

---
+++ src/main/java/com/example/jaal/m0/s4/ReverseInPlace.java
@@ -0,0 +1,52 @@
+/*
+ * Java - Algorithms
+ * 
+ * https://github.com/egalli64/jaal
+ */
+package com.example.jaal.m0.s4;
+
+import java.util.Arrays;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Reversing an array in place
+ */
+public class ReverseInPlace {
+    private static final Logger log = LoggerFactory.getLogger(ReverseInPlace.class);
+
+    /**
+     * Swap all the elements in the array, from the extreme positions converging to
+     * the center
+     * 
+     * @param data the array to be reversed
+     */
+    public static void reverse(int[] data) {
+        log.info("Parameter is {}", Arrays.toString(data));
+        if (data == null) {
+            return;
+        }
+
+        for (int i = 0; i < data.length / 2; i++) {
+            int tmp = data[i];
+            data[i] = data[data.length - i - 1];
+            data[data.length - i - 1] = tmp;
+        }
+
+        log.info("Reversed parameter is {}", Arrays.toString(data));
+    }
+
+    /**
+     * Smoke test
+     * 
+     * @param args not used
+     */
+    public static void main(String[] args) {
+        int[] data = { 3, 54, 12, 99 };
+        System.out.println("Original data is " + Arrays.toString(data));
+
+        reverse(data);
+        System.out.println("After in-place reverse is " + Arrays.toString(data));
+    }
+}
---
+++ src/test/java/com/example/jaal/m0/s4/ReverseInPlaceTest.java
@@ -0,0 +1,51 @@
+/*
+ * Java - Algorithms
+ * 
+ * https://github.com/egalli64/jaal
+ */
+package com.example.jaal.m0.s4;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.Test;
+
+class ReverseInPlaceTest {
+    @Test
+    void reverseNull() {
+        int[] input = null;
+        ReverseInPlace.reverse(input);
+        assertThat(input).isNull();
+    }
+
+    @Test
+    void reverseEmpty() {
+        int[] input = {};
+        ReverseInPlace.reverse(input);
+        assertThat(input).isEmpty();
+    }
+
+    @Test
+    void reverseSingle() {
+        int[] input = { 42 };
+        ReverseInPlace.reverse(input);
+        assertThat(input).contains(42);
+    }
+
+    @Test
+    void reverseEvenSize() {
+        int[] input = { 42, 12, 43, 65 };
+        int[] expected = { 65, 43, 12, 42 };
+
+        ReverseInPlace.reverse(input);
+        assertThat(input).isEqualTo(expected);
+    }
+
+    @Test
+    void reverseOddSize() {
+        int[] input = { 42, 12, 43, 65, 81 };
+        int[] expected = { 81, 65, 43, 12, 42 };
+
+        ReverseInPlace.reverse(input);
+        assertThat(input).isEqualTo(expected);
+    }
+}
