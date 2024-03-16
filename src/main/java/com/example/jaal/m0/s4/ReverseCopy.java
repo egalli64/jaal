49d705284198c75fd1f51b42992c2b3ce242ad30
Emanuele <egalli64@gmail.com>
2024-03-16 19:12:01 +0100

    reverse - copy

---
+++ src/main/java/com/example/jaal/m0/s4/ReverseCopy.java
@@ -0,0 +1,54 @@
+/*
+ * Java - Algorithms
+ * 
+ * https://github.com/egalli64/jaal
+ */
+package com.example.jaal.m0.s4;
+
+import java.util.Arrays;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Generate a reverse-copy of an array
+ */
+public class ReverseCopy {
+    private static final Logger log = LoggerFactory.getLogger(ReverseCopy.class);
+
+    /**
+     * Copy each element from the input array, from left to right, to the same-sized
+     * output one, from right to left
+     * 
+     * @param data the array to be reversed
+     * @return the generated reversed array
+     */
+    public static int[] reverse(int[] data) {
+        log.info("Parameter is {}", Arrays.toString(data));
+        if (data == null || data.length == 0) {
+            return data;
+        }
+
+        int[] result = new int[data.length];
+
+        for (int i = 0; i < data.length; i++) {
+            result[data.length - i - 1] = data[i];
+        }
+
+        log.info("The result is {}", Arrays.toString(result));
+        return result;
+    }
+
+    /**
+     * Smoke test
+     * 
+     * @param args not used
+     */
+    public static void main(String[] args) {
+        int[] data = { 3, 54, 12, 99 };
+        System.out.println("Original data is " + Arrays.toString(data));
+
+        int[] reverted = reverse(data);
+        System.out.println("After in-place reverse is " + Arrays.toString(reverted));
+    }
+}
---
+++ src/test/java/com/example/jaal/m0/s4/ReverseCopyTest.java
@@ -0,0 +1,50 @@
+/*
+ * Java - Algorithms
+ * 
+ * https://github.com/egalli64/jaal
+ */
+package com.example.jaal.m0.s4;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.Test;
+
+class ReverseCopyTest {
+    @Test
+    void reverseNull() {
+        int[] reversed = ReverseCopy.reverse(null);
+        assertThat(reversed).isNull();
+    }
+
+    @Test
+    void reverseEmpty() {
+        int[] input = {};
+        int[] reversed = ReverseCopy.reverse(input);
+        assertThat(reversed).isEmpty();
+    }
+
+    @Test
+    void reverseSingle() {
+        int[] input = { 42 };
+        int[] reversed = ReverseCopy.reverse(input);
+        assertThat(reversed).contains(input);
+    }
+
+    @Test
+    void reverseEvenSize() {
+        int[] input = { 42, 12, 43, 65 };
+        int[] expected = { 65, 43, 12, 42 };
+
+        int[] reversed = ReverseCopy.reverse(input);
+        assertThat(reversed).isEqualTo(expected);
+    }
+
+    @Test
+    void reverseOddSize() {
+        int[] input = { 42, 12, 43, 65, 81 };
+        int[] expected = { 81, 65, 43, 12, 42 };
+
+        int[] reversed = ReverseCopy.reverse(input);
+        assertThat(reversed).isEqualTo(expected);
+    }
+}
