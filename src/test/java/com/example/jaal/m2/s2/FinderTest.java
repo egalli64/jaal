43577c5e84527592741ced2312a3d8d99ffe52f8
Emanuele <egalli64@gmail.com>
2024-08-06 10:42:19 +0200

    more tests

--- src/test/java/com/example/jaal/m2/s2/FinderTest.java
+++ src/test/java/com/example/jaal/m2/s2/FinderTest.java
@@ -17,38 +17,49 @@ class FinderTest {
     private final int[] data = { 42, -12, 1, 21, 0, 99, 21, 5 };
 
     @ParameterizedTest
-    @CsvSource({ "21,3", "-12,1", "1024,-1" })
-    void indexOf(int value, int expected) {
+    @CsvSource({ "42,0", "21,3", "-12,1" })
+    void indexOfWhenPresentThenPosition(int value, int expected) {
         assertThat(Finder.indexOf(data, value)).isEqualTo(expected);
     }
 
     @Test
-    void indexOfOnNull() {
+    void indexOfWhenMissingThenMinusOne() {
+        int value = 1024;
+        assertThat(Finder.indexOf(data, value)).isEqualTo(-1);
+    }
+
+    @Test
+    void indexOfOnWhenNullThenNPE() {
         assertThatNullPointerException().isThrownBy(() -> Finder.indexOf(null, 0));
     }
 
     @ParameterizedTest
-    @ValueSource(ints = { 21, -12, 0 })
-    void containsFound() {
-        int value = 21;
+    @ValueSource(ints = { 42, 21, -12, 0, 5 })
+    void containsWhenPresentThenTrue(int value) {
         assertThat(Finder.contains(data, value)).isTrue();
     }
 
     @Test
-    void containsNotFound() {
+    void containsWhenMissingThenFalse() {
         int value = 1024;
         assertThat(Finder.contains(data, value)).isFalse();
     }
 
     @Test
-    void containsOnNull() {
+    void containsWhenNullThenNPE() {
         assertThatNullPointerException().isThrownBy(() -> Finder.contains(null, 0));
     }
 
     @ParameterizedTest
-    @CsvSource({ "21,6", "-12,1", "1024,-1" })
-    void lastIndexOfFound(int value, int expected) {
+    @CsvSource({ "42,0", "21,6", "-12,1" })
+    void lastIndexOfWhenPresentThenPosition(int value, int expected) {
         assertThat(Finder.lastIndexOf(data, value)).isEqualTo(expected);
+    }
+
+    @Test
+    void lastIndexOfWhenMissingThenMinusOne() {
+        int value = 1024;
+        assertThat(Finder.lastIndexOf(data, value)).isEqualTo(-1);
     }
 
     @Test
