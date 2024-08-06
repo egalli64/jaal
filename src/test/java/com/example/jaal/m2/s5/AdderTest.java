91c045a52e9545b25484ff642a5170c3771c2b9f
Emanuele <egalli64@gmail.com>
2024-08-06 11:07:56 +0200

    clarify

--- src/test/java/com/example/jaal/m2/s5/AdderTest.java
+++ src/test/java/com/example/jaal/m2/s5/AdderTest.java
@@ -8,33 +8,35 @@ package com.example.jaal.m2.s5;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatNullPointerException;
 
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 class AdderTest {
     @Test
-    void sumPlain() {
+    void sumWhenSimpleInput() {
         int[] data = { 1, 2, 3, -12, 3, 2, 1 };
         assertThat(Adder.sum(data)).isZero();
     }
 
     @Test
-    void sumEmpty() {
+    void sumWhenEmptyThenZero() {
         assertThat(Adder.sum(new int[] {})).isEqualTo(0);
     }
 
     @Test
-    void sumNull() {
+    void sumWhenNullThenNPE() {
         assertThatNullPointerException().isThrownBy(() -> Adder.sum(null));
     }
 
     /**
-     * !!! Ensure that in case of overflow sum() returns a wrong value !!!
+     * It is not meant to work!
      * 
      * TODO: write more robust versions of sum()
      */
     @Test
-    void sumOverflow() {
+    @Disabled
+    void sumWhenBigResultThenOverflow() {
         int[] data = { 2_000_000_000, 2_000_000_000 };
-        assertThat(Adder.sum(data)).isLessThan(0);
+        assertThat(Adder.sum(data)).isEqualTo(4_000_000_000L);
     }
 }
