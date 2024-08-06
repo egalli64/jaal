c0d49b80969e65531345a90c481b3970dc5410db
Emanuele <egalli64@gmail.com>
2024-08-06 10:19:21 +0200

    clarify

--- src/test/java/com/example/jaal/m1/s4/ReverseInPlaceTest.java
+++ src/test/java/com/example/jaal/m1/s4/ReverseInPlaceTest.java
@@ -11,28 +11,28 @@ import org.junit.jupiter.api.Test;
 
 class ReverseInPlaceTest {
     @Test
-    void reverseNull() {
+    void reverseWhenNull() {
         int[] input = null;
         ReverseInPlace.reverse(input);
         assertThat(input).isNull();
     }
 
     @Test
-    void reverseEmpty() {
+    void reverseWhenEmpty() {
         int[] input = {};
         ReverseInPlace.reverse(input);
         assertThat(input).isEmpty();
     }
 
     @Test
-    void reverseSingle() {
+    void reverseWhenSingle() {
         int[] input = { 42 };
         ReverseInPlace.reverse(input);
         assertThat(input).contains(42);
     }
 
     @Test
-    void reverseEvenSize() {
+    void reverseWhenEvenSize() {
         int[] input = { 42, 12, 43, 65 };
         int[] expected = { 65, 43, 12, 42 };
 
@@ -41,7 +41,7 @@ class ReverseInPlaceTest {
     }
 
     @Test
-    void reverseOddSize() {
+    void reverseWhenOddSize() {
         int[] input = { 42, 12, 43, 65, 81 };
         int[] expected = { 81, 65, 43, 12, 42 };
 
