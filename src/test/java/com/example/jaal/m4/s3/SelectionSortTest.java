a2b6dc785433d2e9c3dc7bda7b15bf18688aa69b
Emanuele <egalli64@gmail.com>
2024-08-07 11:22:35 +0200

    clarify

--- src/test/java/com/example/jaal/m4/s3/SelectionSortTest.java
+++ src/test/java/com/example/jaal/m4/s3/SelectionSortTest.java
@@ -12,7 +12,7 @@ import org.junit.jupiter.api.Test;
 
 class SelectionSortTest {
     @Test
-    void plainPlain() {
+    void plainWhenPlain() {
         int[] data = { 7, 1, 0, 5, 4, 3 };
         int[] expected = { 0, 1, 3, 4, 5, 7 };
 
@@ -21,7 +21,7 @@ class SelectionSortTest {
     }
 
     @Test
-    void plainOrdered() {
+    void plainWhenOrdered() {
         int[] data = { 0, 1, 3, 4, 5, 7 };
         int[] expected = { 0, 1, 3, 4, 5, 7 };
 
@@ -30,7 +30,7 @@ class SelectionSortTest {
     }
 
     @Test
-    void plainEmpty() {
+    void plainWhenEmpty() {
         int[] data = {};
         int[] expected = {};
 
@@ -39,12 +39,12 @@ class SelectionSortTest {
     }
 
     @Test
-    void plainNull() {
+    void plainWhenNullThenNPE() {
         assertThatNullPointerException().isThrownBy(() -> SelectionSort.plain(null));
     }
 
     @Test
-    void lessWritePlain() {
+    void lessWriteWhenPlain() {
         int[] data = { 7, 1, 0, 5, 4, 3 };
         int[] expected = { 0, 1, 3, 4, 5, 7 };
 
@@ -53,7 +53,7 @@ class SelectionSortTest {
     }
 
     @Test
-    void lessWriteOrdered() {
+    void lessWriteWhenOrdered() {
         int[] data = { 0, 1, 3, 4, 5, 7 };
         int[] expected = { 0, 1, 3, 4, 5, 7 };
 
